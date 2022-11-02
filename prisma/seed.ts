75b1fc570ab17589860c2b40be3c4bd8867a9769
danyllo-aniceto <danyllo.aniceto.carvalho@gmail.com>
2022-11-02 20:14:42 -0300

    Aula 02 - Criando rotas e acessos

---
+++ prisma/seed.ts
@@ -0,0 +1,60 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      name: 'Danyllo',
+      email: 'danyllo@gmail.com',
+      avatarUrl: 'https://github.com/danyllo-aniceto.png'
+    }
+  })
+
+  const pool = await prisma.pool.create({
+    data: {
+      title: 'Example Pool',
+      code: '123456',
+      ownerId: user.id,
+
+      participants: {
+        create: {
+          userId: user.id
+        }
+      }
+    }
+  })
+
+  await prisma.game.create({
+    data: {
+      date: '2022-11-02T17:32:20.807Z',
+      firstTeamCountryCode: 'DE',
+      secondTeamCountryCode: 'BR'
+    }
+  })
+
+  await prisma.game.create({
+    data: {
+      date: '2022-11-03T17:32:20.807Z',
+      firstTeamCountryCode: 'AR',
+      secondTeamCountryCode: 'BR',
+
+      guesses: {
+        create: {
+          firstTeamPoints: 2,
+          secondTeamPoints: 1,
+          participant: {
+            connect: {
+              userId_poolId: {
+                userId: user.id,
+                poolId: pool.id
+              }
+            }
+          }
+        }
+      }
+    }
+  })
+}
+
+main()
